b84631ab8e7991bc58413e5021538b5c323bb96a
unknown <s36651786@gmail.com>
2023-02-11 01:13:09 +0600

    problem solve two

--- problem-one.js
+++ problem-one.js
@@ -1,3 +1,6 @@
+// first problem solve ===========================================
+
+/*
 const arr = [1,2,3,4]
 function sum(num, target){
   for(let i = 0; i < num.length; i++){
@@ -12,3 +15,22 @@ function sum(num, target){
 }
 
 sum(arr, 6)
+*/
+
+
+// second problem =========================
+
+/*
+var removeDuplicates = function(nums) {
+  let k = 0;
+  for (let i = 0; i <= nums.length; i++) {
+    if (nums[i] !== nums[k]) {
+      k++;
+      nums[k] = nums[i];
+    }
+  }
+  return k
+}
+
+removeDuplicates([0,0,1,1,1,2,2,3,3,4])
+*/

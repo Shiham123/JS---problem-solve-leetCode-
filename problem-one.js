ef5f4dbb3236b20cc52816e3e77f9fb50c3a924a
unknown <s36651786@gmail.com>
2023-02-11 10:42:38 +0600

    problem one alternative add

--- problem-one.js
+++ problem-one.js
@@ -1,10 +1,12 @@
 // first problem solve ===========================================
 
-/*
+
 const arr = [1,2,3,4]
 function sum(num, target){
   for(let i = 0; i < num.length; i++){
+  
     for(let j = i + 1; j < num.length; j++){
+    
       let pointer = arr[i] + arr[j]
       if(pointer === target){
         return [i, j]
@@ -15,12 +17,26 @@ function sum(num, target){
 }
 
 sum(arr, 6)
-*/
+
+// Frist problem solution an alternative solution
+
+function sumIndex(arr, value){
+  for(let i = 0; i < arr.length; i++){
+    let remainValue = value - arr[i] 
+
+    for(let j = i + 1; j < arr.length; j++){
+      if(remainValue === arr[j]) return [i, j]
+    }
+  }
+  return false
+}
+
+sumIndex([1,2,3,4],7)
 
 
 // second problem =========================
 
-/*
+
 var removeDuplicates = function(nums) {
   let k = 0;
   for (let i = 0; i <= nums.length; i++) {
@@ -33,4 +49,5 @@ var removeDuplicates = function(nums) {
 }
 
 removeDuplicates([0,0,1,1,1,2,2,3,3,4])
-*/
+
+
